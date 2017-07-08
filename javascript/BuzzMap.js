e07d827ecbcc808bb9c7fe90104b0df902e9f068
Antony Videtta <antony@springtimesoft.co.nz>
2017-07-08 23:00:59 +1200

    Add some more instructions

--- javascript/BuzzMap.js
+++ javascript/BuzzMap.js
@@ -1,5 +1,8 @@
 (function(window, google, List) {
 
+ // Buzz map is a library which is attached & accessable from the window object
+ // It takes bits and pieces from the google api & other librarys to contruct markers to the google map 	
+
   var BuzzMap = (function() {
 
     function BuzzMap( element, opts ) { // set up map
@@ -44,13 +47,15 @@
           this.markerClusterer.addMarker(marker);
         }
 
-        this._addMarker(marker);
+        this._addMarker(marker); // add marker to markers array ( to keep track of it)
 
         if (opts.events) {
-          this._attachEvents(marker, opts.events);
+
+          this._attachEvents(marker, opts.events); // add events to marker
+
         }
 
-        if (opts.content) {
+        if (opts.content) { // add an info window to the marker 
 
           this._on({
             obj: marker,
@@ -83,15 +88,15 @@
 
       },
 
-      _addMarker: function(marker) { 
+      _addMarker: function(marker) { // add marker to marker array
         this.markers.add(marker);
       },
 
-      findBy: function(callback) {
+      findBy: function(callback) { // find marker 
         this.markers.find(callback);
       },
 
-      removeBy: function(callback) {
+      removeBy: function(callback) { // remove marker 
         var self = this;
         self.markers.find(callback, function(markers) {
           markers.forEach(function(marker) {
@@ -104,7 +109,7 @@
         });
       },
 
-      _createMarker: function(opts) {
+      _createMarker: function(opts) { // create marker 
         opts.map = this.gMap;
         return new google.maps.Marker(opts);
       }
@@ -115,8 +120,10 @@
 
   }());
 
-  BuzzMap.create = function(element, opts) {
-    return new BuzzMap(element, opts);
+  BuzzMap.create = function(element, opts) { 
+
+    return new BuzzMap(element, opts); // create instance of google map 
+
   };
 
   window.BuzzMap = BuzzMap;
--- javascript/script.js
+++ javascript/script.js
@@ -3,10 +3,16 @@
   // map options
   var options = BuzzMap.MAP_OPTIONS,
   element = document.getElementById('map-canvas'),
-  // map
+
+  // create the map
   map = BuzzMap.create(element, options);
 
-  var marker2 = map.addMarker({
+  // this is where you set all the properties for each marker 
+  // for example, you could grab a collection of data from an array 
+  // loop through it & display all the various lat / lng onto the map, 
+  // change the icon based on the type of data, set up custom info etc
+
+  var marker = map.addMarker({
 	lat: -36.857975, 
 	lng: 174.721354,
     draggable: true,
